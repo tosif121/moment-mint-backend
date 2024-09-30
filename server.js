51b23f4bbaa98a9d50123a30157986c8b063aa8c
tossi <tosifraza8867@gmail.com>
2024-10-01 02:20:20 +0530

    cors

--- server.js
+++ server.js
@@ -4,32 +4,27 @@ const userRoutes = require('./src/routes/users.routes');
 const postRoutes = require('./src/routes/post.routes');
 const imageRoutes = require('./src/routes/upload.routes');
 const otpRoutes = require('./src/routes/otp.routes');
+const app = express();
 const dotenv = require('dotenv').config();
 
-const app = express();
-
-// Configure CORS
 app.use(
   cors({
-    origin: ['http://15.207.26.134', 'http://localhost:3000'],
-    methods: ['GET', 'POST'],
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
 app.use(express.json());
 
-// Define routes
 app.use('/api', userRoutes, imageRoutes, postRoutes, otpRoutes);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ status: false, message: 'Internal Server Error' });
 });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const server = app.listen(process.env.PORT || 5000, function (error) {
+  if (error) return new Error(error);
+  console.log('Server is running on http://localhost:' + (process.env.PORT || 5000));
 });
